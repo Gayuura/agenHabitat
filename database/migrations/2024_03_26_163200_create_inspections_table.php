7c74d34685fd8f0926d925dbbb1dc236e416c5af
Yacine Habbak <151452334+Gayuura@users.noreply.github.com>
2024-04-02 16:41:53 +0200

    BDD Migration

---
+++ database/migrations/2024_03_26_163200_create_inspections_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('inspections', function (Blueprint $table) {
+            $table->id();
+            $table->string('libellé');
+            $table->string('adresse');
+            $table->dateTime('date_et_heure');
+            $table->string('nomLocataire');
+            $table->integer('numeroLocataire');
+            $table->boolean('conformité');
+            $table->boolean('état');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('inspections');
+    }
+};
---
+++ database/migrations/2024_03_27_083428_create_rapports_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rapports', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('locataire_id')->constrained()->onDelete('cascade');
+            $table->foreignId('logement_id')->constrained()->onDelete('cascade');
+            $table->string('signature_locataire')->nullable();
+            $table->string('signature_inspecteur')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rapports');
+    }
+};
